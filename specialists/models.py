0fceb346f26d114b9e65f5d775a117bc468d0643
oxxxipoint <64486523+oxxxipoint@users.noreply.github.com>
2020-04-28 23:20:27 +0300

    Update models.py

--- specialists/models.py
+++ specialists/models.py
@@ -20,7 +20,7 @@ class Specialist(models.Model):
         return sum / maxQ
 
     def setMarks(self):
-        file = open('specialists/static/criteria/estimations.txt', 'r')
+        file = open('specialists/static/criteria/dick.txt', 'r')
         marks = file.read()
         file.close()
         nums = [0, 0.25, 0.5, 0.75, 1]
